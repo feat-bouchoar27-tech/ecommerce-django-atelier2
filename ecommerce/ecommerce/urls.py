1f0183a82d1ea87707e54432a8b99f4d2f874a14
feat-bouchoar27-tech <fe.at-bouchoar.27@eigsica.ma>
2026-05-31 15:03:24 +0100

    Fix indentation and remove redundant imports in urls.py

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -4,15 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
-]from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
