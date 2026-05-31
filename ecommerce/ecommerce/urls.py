aa96bea6cdfc50e648e336511cadfae1ab8e59bf
feat-bouchoar27-tech <fe.at-bouchoar.27@eigsica.ma>
2026-05-31 14:44:36 +0100

    Update urls.py

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('products/', include('products.urls')),
+]from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
